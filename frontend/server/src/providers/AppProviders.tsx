26c78a7e585d00faff3ba843ee59f0095d4769a1
CowardHedgehog <c6400996@st.kanazawa-it.ac.jp>
2025-05-28 17:03:40 +0900

    一部ファイルの変更の取り消し

--- frontend/server/src/providers/AppProviders.tsx
+++ frontend/server/src/providers/AppProviders.tsx
@@ -9,26 +9,23 @@ import { LoginUserProvider } from "./LoginUserProvider";
 import { ThemeProvider } from "./ThemeProviders";
 
 // MathJaxの設定を動的にインポート
-const MathJaxSetup = dynamic(
-	() => import("../components/shared/MathJax").then((m) => m.MathJaxSetup),
-	{ ssr: false },
-);
+const MathJaxSetup = dynamic(() => import("../components/shared/MathJax").then((m) => m.MathJaxSetup), { ssr: false });
 
 // Emotionのクライアントキャッシュを作成
 const clientSideEmotionCache = createEmotionCache();
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-	return (
-		<CacheProvider value={clientSideEmotionCache}>
-			<CookiesProvider>
-				<MathJaxSetup>
-					<LoginUserProvider>
-						<ThemeProvider>
-							<main>{children}</main>
-						</ThemeProvider>
-					</LoginUserProvider>
-				</MathJaxSetup>
-			</CookiesProvider>
-		</CacheProvider>
-	);
+  return (
+    <CacheProvider value={clientSideEmotionCache}>
+      <CookiesProvider>
+        <MathJaxSetup>
+          <LoginUserProvider>
+            <ThemeProvider>
+              <main>{children}</main>
+            </ThemeProvider>
+          </LoginUserProvider>
+        </MathJaxSetup>
+      </CookiesProvider>
+    </CacheProvider>
+  );
 }
--- frontend/server/src/providers/LoginUserProvider.tsx
+++ frontend/server/src/providers/LoginUserProvider.tsx
@@ -1,60 +1,48 @@
-import {
-	type Dispatch,
-	type SetStateAction,
-	createContext,
-	useEffect,
-	useState,
-} from "react";
+import { type Dispatch, type SetStateAction, createContext, useEffect, useState } from "react";
 import { useCookies } from "react-cookie";
 import axios from "../lib/axios";
 
 import type { User } from "../types/api/auth/user";
 
 export type LoginUserContextType = {
-	loginUser: User | null;
-	setLoginUser: Dispatch<SetStateAction<User | null>>;
-	isLoadingUser: boolean;
-	// isAuthenticated: boolean; 認証状況フラグ
+  loginUser: User | null;
+  setLoginUser: Dispatch<SetStateAction<User | null>>;
+  isLoadingUser: boolean;
+  // isAuthenticated: boolean; 認証状況フラグ
 };
 
-export const LoginUserContext = createContext<LoginUserContextType>(
-	{} as LoginUserContextType,
-);
+export const LoginUserContext = createContext<LoginUserContextType>({} as LoginUserContextType);
 
 export const LoginUserProvider = (props: { children: React.ReactNode }) => {
-	const { children } = props;
-	const [loginUser, setLoginUser] = useState<User | null>(null);
-	const [isLoadingUser, setIsLoadingUser] = useState(true);
-	const [cookies] = useCookies(["token"]);
+  const { children } = props;
+  const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [isLoadingUser, setIsLoadingUser] = useState(true);
+  const [cookies] = useCookies(["token"]);
 
-	useEffect(() => {
-		const fetchUser = async () => {
-			if (cookies.token && !loginUser) {
-				setIsLoadingUser(true);
-				await axios
-					.get("/home_profile")
-					.then((res) => {
-						if (res.status === 200) {
-							setLoginUser(res.data);
-						}
-					})
-					.catch((error) => {
-						console.error("Error fetching user data:", error);
-					})
-					.finally(() => {
-						setIsLoadingUser(false);
-					});
-			} else {
-				setIsLoadingUser(false);
-			}
-		};
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (cookies.token && !loginUser) {
+        setIsLoadingUser(true);
+        await axios
+          .get("/home_profile")
+          .then((res) => {
+            if (res.status === 200) {
+              setLoginUser(res.data);
+            }
+          })
+          .catch((error) => {
+            console.error("Error fetching user data:", error);
+          })
+          .finally(() => {
+            setIsLoadingUser(false);
+          });
+      } else {
+        setIsLoadingUser(false);
+      }
+    };
 
-		fetchUser();
-	}, [cookies.token, loginUser]);
+    fetchUser();
+  }, [cookies.token, loginUser]);
 
-	return (
-		<LoginUserContext value={{ loginUser, setLoginUser, isLoadingUser }}>
-			{children}
-		</LoginUserContext>
-	);
+  return <LoginUserContext value={{ loginUser, setLoginUser, isLoadingUser }}>{children}</LoginUserContext>;
 };
