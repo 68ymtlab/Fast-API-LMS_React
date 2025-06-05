4994035b7185553e66fd58126ddf1a75d9711305
OgiharaDaisuke <c1119233@planet.kanazawa-it.ac.jp>
2025-06-05 14:02:15 +0900

    色の変更

--- frontend/server/src/app/(students)/home/page.tsx
+++ frontend/server/src/app/(students)/home/page.tsx
@@ -256,7 +256,6 @@ export const StudentHome = () => {
 
   return (
     <>
-      <StudentHeader />
       <main>
         <div className="flex flex-col items-start justify-start min-h-screen bg-gray-100 pt-20">
           <div className="container mx-auto px-8 py-8 max-w-7xl">
